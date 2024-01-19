b70af07c85253012719340c467e38c8ffeaf218c
EchoRussell <137955529+EchoRussell@users.noreply.github.com>
2024-01-18 21:08:39 -0500

    Html implemented. Boostrap updated. Footer added.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,15 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <p className="personal-info">
+      This project was developed by Alana Russell and is available as an
+      open-source repository on&nbsp;
+      <a href="https://github.com/EchoRussell/weather-app-react">Github </a>. It
+      is also hosted on &nbsp;
+      <a href="https://enchanting-pastelito-f7d5d7.netlify.app/">Netlify</a>
+    </p>
+  );
 }
 
 export default App;
