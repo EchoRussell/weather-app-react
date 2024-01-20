0811a2b799016a7ef2ec88579e6fc3009b4e5265
EchoRussell <137955529+EchoRussell@users.noreply.github.com>
2024-01-20 13:00:50 -0500

    App Css implications

--- src/Weather.js
+++ src/Weather.js
@@ -10,6 +10,7 @@ export default function Weather() {
               type="search"
               placeholder="Enter a city.."
               className="form-control "
+              autoFocus="on"
             />
           </div>
           <div className="col-3">
@@ -22,24 +23,26 @@ export default function Weather() {
         </div>
       </form>
       <h1>New York</h1>
-      <ul>
-        <li>Wednesday 07:00</li>
-        <li>Mostly cloudy</li>
+      <ul className="time-description">
+        <li>Wednesday : 07:00</li>
+        <li>Mostly : cloudy</li>
       </ul>
 
       <div className="row">
         <div className="col-6">
           <img
+            className="cloud-icon"
             src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png"
             alt="clouds"
           ></img>
-          <span> -9 °C</span>
+          <span className="degree">-9</span>
+          <span className="temp-unit">°C</span>
         </div>
         <div className="col-6">
           <ul>
-            <li>Precipitation: 0% </li>
-            <li>Humidity: 61%</li>
-            <li> Wind: 14 km/h</li>
+            <li>Precipitation : 0% </li>
+            <li>Humidity : 61%</li>
+            <li> Wind : 14 km/h</li>
           </ul>
         </div>
       </div>
