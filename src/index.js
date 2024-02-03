367e80cd5448537e697d863c76b3bb01fc075396
EchoRussell <137955529+EchoRussell@users.noreply.github.com>
2024-02-02 19:18:44 -0500

    Use Effect to change the state of the component

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Weather defaultCity="Paris" />
+    <Weather defaultCity="Toronto" />
     <App />
   </React.StrictMode>
 );
